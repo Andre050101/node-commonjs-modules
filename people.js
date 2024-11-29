6b2d9d07285499ac37499e70ada9ef849699fa8d
Andre050101 <agiancristiano90@gmail.com>
2024-11-29 15:43:00 +0100

    complete

--- people.js
+++ people.js
@@ -1,9 +1,9 @@
 const name = require('./names.js');
 const hobby = require('./hobbies.js');
 
-const firstName = process.argv[2];
-const lastName = process.argv[3];
-const [hobby1, hobby2, hobby3] = [process.argv[4], process.argv[5], process.argv[6]];
+const firstName = process.argv[2] || "name";
+const lastName = process.argv[3] || "surname";
+const [hobby1, hobby2, hobby3] = [process.argv[4] || "Hobby1", process.argv[5] || "Hobby2", process.argv[6] || "hobby3"];
 function createPerson() {
     const fullName = name(firstName, lastName);
     const hobbies = hobby(hobby1, hobby2, hobby3);
